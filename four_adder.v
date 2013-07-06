fb1c77b93e8bdfd5843c77139c172e35495537b9
ravan <ravan@lanka>
2013-07-06 10:59:31 +0530

    large four adder changed to carry select adder

--- four_adder.v
+++ four_adder.v
@@ -8,7 +8,46 @@ output [33:0] sum_four
 );
 
 wire [33:0] sum_four_unsigned ;
-assign sum_four_unsigned = in_data_3 + in_data_2 + in_data_1 + in_data_0 ;
+wire [34:0] sum_01 ;
+wire [34:0] sum_23 ;
+wire [36:0] sum_1234 ;
+assign sum_four_unsigned = sum_1234[36:0] ;
 assign sum_four = poly_sign ? (~sum_four_unsigned)+1 : sum_four_unsigned ;
+
+
+n_bit_csa_adder #(
+    .IN_DATAWIDTH(34)
+)
+adder_01
+(
+    .in1({1'b0,in_data_0}),        
+    .in2({1'b0,in_data_1}),   
+    .cin(1'b0),
+    .sum(sum_01)
+);
+
+n_bit_csa_adder #(
+    .IN_DATAWIDTH(34)
+)
+adder_23
+(
+    .in1({1'b0,in_data_2}),    
+    .in2({1'b0,in_data_3}),   
+    .cin(1'b0),
+    .sum(sum_23)
+);
+
+
+n_bit_csa_adder #(
+    .IN_DATAWIDTH(36)
+)
+adder_1234
+(
+    .in1({1'b0,sum_01}),        
+    .in2({1'b0,sum_23}),   
+    .cin(1'b0),
+    .sum(sum_1234)
+);
+
 
 endmodule 
